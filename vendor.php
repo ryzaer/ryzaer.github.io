1ad644d050b1e5f10c34dcbb5716b0f85dd2cbb1
ryzaer <ryzaer@live.com>
2024-12-07 21:12:05 +0700

    up body

--- vendor.php
+++ vendor.php
@@ -1,3 +1,4 @@
+
 <?php
 // run php vendor.php
 class jquery {
@@ -5,19 +6,36 @@ class jquery {
     function uname($str){
         return str_replace(" ","-",preg_replace('/\s+/',' ',strtolower($str)));
     }
+    function htmbody($title){
+        return implode('',[
+"<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>$title</title>
+</head>
+<body>
+    <h2>Hello, this is $title</h2>
+</body>
+</html>"
+        ]);
+    }
     static function page(...$args){
         if(!self::$stmt)
             self::$stmt = new jquery();
-        foreach ($args as $val) {
-            $wname=__DIR__ .'/'.self::$stmt->uname($val[0]).'.html';
-            var_dump($wname);
-            // file_exists($wname) || file_put_contents($wname,"this is {$val[0]} page");    
+        foreach ($args as $num => $val) {
+            $wname= __DIR__ .'/'.self::$stmt->uname($num > 0 ? $val[0] : 'index').'.html';
+            $hbody= self::$stmt->htmbody($val[0]);
+            // var_dump($wname);
+            file_exists($wname) || file_put_contents($wname,$hbody);    
         }
     }
+
 }
 
 jquery::page(
-    ["test"],
-    ["dATA CONTOH"],
-    ["ikan tongkol"]
+    ["Mukadimah"],
+    ["Data Contoh"],
+    ["About"]
 );
