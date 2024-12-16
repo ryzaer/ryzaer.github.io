1ba1f1a2b6f02a6c281076c9efbd4eff470df73e
ryzaer <ryzaer@live.com>
2024-12-16 20:46:47 +0700

    up pages

--- vendor.php
+++ vendor.php
@@ -1,8 +1,9 @@
 
 <?php
 // run php vendor.php
-class jquery {
+class templateSPA {
     private static $stmt;
+    
     function uname($str){
         return str_replace(" ","-",preg_replace('/\s+/',' ',strtolower($str)));
     }
@@ -10,20 +11,39 @@ class jquery {
         return implode('',[
 "<!DOCTYPE html>
 <html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>$title</title>
-</head>
-<body>
-    <h2>Hello, this is $title</h2>
-</body>
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>$title</title>
+    </head>
+    <body>
+        <h2>Hello, this is $title</h2>
+    </body>
+    <script>
 </html>"
         ]);
     }
-    static function page(...$args){
+    function dirpages(){
+
+    }
+    static function name($str){
         if(!self::$stmt)
-            self::$stmt = new jquery();
+            self::$stmt = new templateSPA();
+        self::$stmt->name = $str;
+    }
+    static function header($str){
+        if(!self::$stmt)
+            self::$stmt = new templateSPA();
+        self::$stmt->header = $str;
+    }
+    static function footer($str){
+        if(!self::$stmt)
+            self::$stmt = new templateSPA();
+        self::$stmt->footer = $str;
+    }
+    static function pages(...$args){
+        if(!self::$stmt)
+            self::$stmt = new templateSPA();
         foreach ($args as $num => $val) {
             $wname= __DIR__ .'/'.self::$stmt->uname($num > 0 ? $val[0] : 'index').'.html';
             $hbody= self::$stmt->htmbody($val[0]);
@@ -33,8 +53,7 @@ class jquery {
     }
 
 }
-
-jquery::page(
+templateSPA::pages(
     ["Mukadimah"],
     ["Data Contoh"],
     ["About"]
